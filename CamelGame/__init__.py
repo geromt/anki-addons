25358ef79f0bd9ec77d80149821090e9837380fc
geromt <gerardomartinez205@gmail.com>
2023-01-09 14:48:57 -0600

    Adding config file with initial values

--- CamelGame/__init__.py
+++ CamelGame/__init__.py
@@ -1,18 +1,41 @@
+import json
+import os.path
 import random
 
-from aqt import gui_hooks
+from aqt import gui_hooks, mw
 from aqt.utils import askUserDialog, showInfo
 
 
 class CamelGame:
-    def __init__(self):
-        self.distance = 0
-        self.natives = -20
-        self.canteen = 3
-        self.tiredness = 0
-        self.thirst = 0
+    def __init__(self, savefile):
+        self.savefile = savefile
+        self.config = mw.addonManager.getConfig(__name__)
+        self.traveled_distance: int
+        self.natives: int
+        self.canteen: int
+        self.thirst: int
+
+        if os.path.exists(self.savefile):
+            self._load_save()
+        else:
+            self.load_init()
+
         self.turns = 0
         self.alive = True
+
+    def _load_save(self):
+        with open(self.savefile, "r") as save:
+            data = json.load(save)
+            self._load(data)
+
+    def load_init(self):
+        self._load(self.config["init_vals"])
+
+    def _load(self, data):
+        self.traveled_distance = data["traveled_distance"]
+        self.natives = data["natives"]
+        self.canteen = data["canteen"]
+        self.thirst = data["thirst"]
 
     def drink(self):
         if self.canteen > 0:
@@ -22,31 +45,34 @@ class CamelGame:
         return False
 
     def walk(self, amount):
-        self.distance += amount
-        self.natives += random.randint(1, 4)
-        self.thirst += random.randint(0, 2)
+        self.traveled_distance += amount
+        self.natives += random.randint(1, 3)
+        self.thirst += random.randint(0, 1)
 
     def run(self, reviewer, card, ease):
         if not self.alive:
             return
 
         self.walk(ease)
-        if self.turns % 10 == 0:
-            bd = askUserDialog(f"Traveled Distance: {self.distance}\nDistance from natives: {self.distance-self.natives}\nDrinks in canteen: {self.canteen}\n",
+        if self.turns % self.config["status_check"] == 0:
+            bd = askUserDialog((f"Traveled Distance: {self.traveled_distance}\n"
+                                f"Distance from natives: {self.traveled_distance-self.natives}\n"
+                                f"Drinks in canteen: {self.canteen}\n"),
                                ["Drink water", "Continue"], title="CamelGame")
+
             response = bd.run()
             if response == "Drink water":
                 self.drink()
 
-        if self.natives >= self.distance:
+        if self.natives >= self.traveled_distance:
             showInfo("You've been captured by the natives. Game Over")
             self.alive = False
-        if self.thirst >= 10:
+        if self.thirst >= self.config["thirst_to_die"]:
             showInfo("You've died from dehydration: Game Over")
             self.alive = False
 
         self.turns += 1
 
 
-camel_game = CamelGame()
+camel_game = CamelGame("savefile.json")
 gui_hooks.reviewer_did_answer_card.append(camel_game.run)
