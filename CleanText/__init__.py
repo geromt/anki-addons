4a00849f20d50b4adc5ded5cbbfb42c70bb34417
gero <gerardomartinez205@gmail.com>
2023-03-18 12:36:54 -0600

    feat: Add CleanText button to editor
    CleanText button only delete <br> tags and replace them by single spaces.

---
+++ CleanText/__init__.py
@@ -0,0 +1,33 @@
+import aqt.editor
+from aqt import gui_hooks
+
+import os.path
+
+PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+def delete_br_tag(text: str) -> str:
+    split_text = text.split("<br>")
+    return " ".join(split_text)
+
+
+def clean_text(editor: aqt.editor.Editor):
+    text = editor.note["Original Text"]
+    editor.note["Original Text"] = delete_br_tag(text)
+
+    # Reload the note window
+    editor.loadNote()
+
+
+def add_clean_text_button(buttons, editor):
+    """Function for 'editor_did_init_buttons' hook to add a button that executes context_cloze"""
+    hotkey = "Ctrl+Alt+C"
+    tooltip = f"Clean the text"
+    return buttons.append(editor.addButton(os.path.join(PATH, "media", "clean.svg"),
+                                           "clean_text",
+                                           clean_text,
+                                           tip=tooltip,
+                                           keys=hotkey))
+
+
+gui_hooks.editor_did_init_buttons.append(add_clean_text_button)
